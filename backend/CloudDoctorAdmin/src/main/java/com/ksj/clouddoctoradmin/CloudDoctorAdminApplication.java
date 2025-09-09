43d4ee2c38afbc72309f8f8cb4b92165655c63a6
DESKTOP-TR03E35\USER <hyoju2259@gmail.com>
2025-09-09 16:03:07 +0900

    reset admin application

---
+++ backend/CloudDoctorAdmin/src/main/java/com/ksj/clouddoctoradmin/CloudDoctorAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ksj.clouddoctoradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDoctorAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDoctorAdminApplication.class, args);
+    }
+
+}
